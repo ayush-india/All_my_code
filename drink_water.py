0d4498aade554f0ad44d64d45e731fc79941a912
ayushmishra <mail81mitthu@gmail.com>
2022-09-22 16:57:40 +0530

    reformating

--- drink_water.py
+++ drink_water.py
@@ -1,32 +1,35 @@
-import time
+import time as t
 from pygame import mixer
 
 mixer.init()
 
-def alarm(k):
-    global n
-    # TODO: Play a sound to drink water
-    while k != 0:
-        k -= 1
-        print(f"Drink water FAST !!!!!! \n Timer will reset in {k} sec ")
-        mixer.music.load('alarm.mp3')
+# Play the alarm sound and the again start the timer
+def alarm(timer):
+    global time
+    while timer != 0:
+        timer -= 1
+        print(f"Drink water FAST !!!!!! \n Timer will reset in {timer} sec ")
+        mixer.music.load("alarm.mp3")
         mixer.music.play()
-        time.sleep(1)
-    timer(n)
-    
-def timer(n):
-    global k
-    while n != 0:
-        print(f"Drink water in {n} sec")
-        n -= 1
-        time.sleep(1)
-
-    alarm(k)
+        t.sleep(1)
+    play_timer(time)
 
 
-# TODO Give the input in hr min and sec format 
-n = int(input("Enter the time for timer to run in sec \n :"))
-k = int(input("Enter the time for alarm in sec \n :"))
+# Run the timer for the given amt of time and the call the alarm func
+def play_timer(time):
+    global timer
+    while time != 0:
+        print(f"Drink water in {time} sec")
+        time -= 1
+        t.sleep(1)
 
-timer(n)
+    alarm(timer)
 
+
+# TODO Give the input in hr min and sec format
+# Input the required variables
+time = int(input("Enter the time for timer to run in sec \n :"))
+timer = int(input("Enter the time for alarm in sec \n :"))
+
+# Call the main method
+play_timer(time)
